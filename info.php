772df55875b1ad705d41c1f49675f9366823ce95
aniollidon <aniollidon@users.noreply.github.com>
2022-11-08 15:02:43 +0100

    Update info.php

--- info.php
+++ info.php
@@ -9,13 +9,13 @@ echo "<a href='?phpinfo'>". phpversion() . "</a><br>";
 
 echo "<b>Git branch </b> <br>";
 chdir("/var/www/html/MP07");
-echo shell_exec("cat .aniol_gitpull/aniol_gitpull-branch.txt");
+echo file_get_contents(".aniol_gitpull/aniol_gitpull-branch.txt");
 
 echo "<b>Git commit info </b> <br>";
-echo shell_exec("cat .aniol_gitpull/aniol_gitpull-gitlog.txt"). "<br>";
+echo file_get_contents(".aniol_gitpull/aniol_gitpull-gitlog.txt");
 
 echo "<b>Git commit files </b> <br>";
-echo shell_exec('cat .aniol_gitpull/aniol_gitpull-files.txt');
+echo file_get_contents(".aniol_gitpull/aniol_gitpull-files.txt");
 
 echo "<br>";
 
@@ -23,5 +23,6 @@ echo "<b>Service status </b> <br>";
 echo shell_exec("systemctl status aniol_gitpull --no-pager"). "<br>";
 
 echo "<b>Service log </b> <br>";
-echo shell_exec("tail -5 /var/log/aniol_gitpull.log"). "<br>";
+echo shell_exec("tail /var/log/aniol_gitpull.log"). "<br>";
 
+echo "</pre>";
