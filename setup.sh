8cb0b44c7da47710e682bf2700f89229edf711b4
aniollidon <aniollidon@users.noreply.github.com>
2022-11-08 18:14:42 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ sudo rm /etc/systemd/system/aniol_gitpull*
 sudo rm /var/www/html/info.php
 
 cd /var/www/html/
-sudo wget https://raw.githubusercontent.com/aniollidon/gitpull-service/main/info.php
+sudo wget https://raw.githubusercontent.com/aniollidon/gitpull-service/main/info.php --no-cache
 cd /etc/systemd/system/
 
-sudo wget https://raw.githubusercontent.com/aniollidon/gitpull-service/main/aniol_gitpull.service
-sudo wget https://raw.githubusercontent.com/aniollidon/gitpull-service/main/aniol_gitpull.sh
-sudo wget https://raw.githubusercontent.com/aniollidon/gitpull-service/main/aniol_gitpull.timer
+sudo wget https://raw.githubusercontent.com/aniollidon/gitpull-service/main/aniol_gitpull.service --no-cache
+sudo wget https://raw.githubusercontent.com/aniollidon/gitpull-service/main/aniol_gitpull.sh --no-cache
+sudo wget https://raw.githubusercontent.com/aniollidon/gitpull-service/main/aniol_gitpull.timer --no-cache
 
 sudo chmod +x aniol_gitpull.sh
 sudo systemctl daemon-reload
