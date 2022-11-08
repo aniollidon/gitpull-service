f2326de092cbba51ef0106d2fd0cc34375ccfb2f
aniollidon <aniollidon@users.noreply.github.com>
2022-11-08 19:19:51 +0100

    Update aniol_gitpull.sh

--- aniol_gitpull.sh
+++ aniol_gitpull.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 date
 cd /var/www/html/MP07
-git clean --force -d -x
-git reset --hard
+git clean --force -d -x --quiet
+git reset --hard --quiet
 git pull
 git log -1 --format=%cd 
-mkdir -p .aniol_gitpull
+mkdir -p .aniol_gitpull 
 git rev-parse --abbrev-ref HEAD > .aniol_gitpull/aniol_gitpull-branch.txt
 git log -1 > .aniol_gitpull/aniol_gitpull-gitlog.txt
 echo "" > .aniol_gitpull/aniol_gitpull-files.txt
