86cdad7ec96ceb258ebc9d92c836aa17b255e529
aniollidon <aniollidon@users.noreply.github.com>
2022-11-08 09:49:37 +0100

    Update aniol_gitpull.sh

--- aniol_gitpull.sh
+++ aniol_gitpull.sh
@@ -7,7 +7,7 @@ git pull
 git log -1 --format=%cd 
 git rev-parse --abbrev-ref HEAD > /var/log/aniol_gitpull-branch.txt
 git log -1 > /var/log/aniol_gitpull-gitlog.txt
-git ls-tree -r --name-only HEAD | while read filename; do  echo "$(git log -1 --format="%ad" -- $filename) $filename"; done  > /var/log/aniol_gitpull-files.txt
+echo "" > /var/log/aniol_gitpull-files.txt
+git ls-tree -r --name-only HEAD | while read filename; do  echo "$(git log -1 --format="%ad" -- $filename) $filename"  >> /var/log/aniol_gitpull-files.txt ; done
 chmod +r /var/log/aniol_gitpull-*
-
 echo //////////////////////
