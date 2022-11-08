1d4008de381503c396294b57e7d405ee16cc78dc
aniollidon <aniollidon@users.noreply.github.com>
2022-11-08 19:58:36 +0100

    Update info.php

--- info.php
+++ info.php
@@ -17,28 +17,28 @@ if(isset($_GET["fulllog"])) die("<pre>" . file_get_contents("/var/log/aniol_gitp
 echo "<pre>";
 
 echo "<b>PHP version </b> <br>";
-echo "<a href='?phpinfo'>". phpversion() . "</a><br>";
+echo "<a href='?phpinfo'>". phpversion() . "</a><br><br>";
 
 echo "<b>MySql version </b> <br>";
-echo clean_lines(shell_exec("mysql --version"))."<br>";
+echo clean_lines(shell_exec("mysql --version"))."<br><br>";
   
 echo "<b>Git branch </b> <br>";
 chdir("/var/www/html/MP07");
-echo clean_lines(file_get_contents(".aniol_gitpull/aniol_gitpull-branch.txt"))."<br>";
+echo clean_lines(file_get_contents(".aniol_gitpull/aniol_gitpull-branch.txt"))."<br><br>";
 
 echo "<b>Git commit info </b> <br>";
-echo clean_lines(file_get_contents(".aniol_gitpull/aniol_gitpull-gitlog.txt"))."<br>";
+echo clean_lines(file_get_contents(".aniol_gitpull/aniol_gitpull-gitlog.txt"))."<br><br>";
 
 echo "<b>Git commit files </b> <br>";
-echo clean_lines(file_get_contents(".aniol_gitpull/aniol_gitpull-files.txt"))."<br>";
+echo clean_lines(file_get_contents(".aniol_gitpull/aniol_gitpull-files.txt"))."<br><br>";
 
 echo "<b>Timer status </b> <br>";
-echo clean_lines(shell_exec("systemctl list-timers aniol_gitpull.timer"), 3)."<br>";
+echo clean_lines(shell_exec("systemctl list-timers aniol_gitpull.timer"), 3)."<br><br>";
   
 echo "<b>Service status </b> <br>";
-echo clean_lines(shell_exec("systemctl status aniol_gitpull --no-pager"))."<br>";
+echo clean_lines(shell_exec("systemctl status aniol_gitpull --no-pager"))."<br><br>";
 
 echo "<b>Service <a href='?fulllog'>log</a> </b> <br>";
-echo clean_lines(shell_exec("tail /var/log/aniol_gitpull.log"))."<br>";
+echo clean_lines(shell_exec("tail /var/log/aniol_gitpull.log"))."<br><br>";
 
 echo "</pre>";
